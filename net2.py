c05a67e7840057f1c325bbca2c312c22b512311b
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-09-30 12:00:09 +0900

    Initial commit

---
+++ net2.py
@@ -0,0 +1,33 @@
+from netmiko import ConnectHandler
+
+switch1 = {
+    'device_type': 'cisco_ios',
+    'ip': '10.1.1.11',
+    'username': 'ccnp',
+    'password': 'cisco'
+}
+
+switch2 = {
+    'device_type': 'cisco_ios',
+    'ip': '10.1.1.12',
+    'username': 'ccnp',
+    'password': 'cisco'
+}
+
+switch3 = {
+    'device_type': 'cisco_ios',
+    'ip': '10.1.1.13',
+    'username': 'ccnp',
+    'password': 'cisco'
+}
+
+devices = [switch1, switch2, switch3]
+
+for device in devices:
+    net_connect = ConnectHandler(**device)
+    for n in range (2,11):
+       print ("Creating VLAN " + str(n))
+       config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
+       output = net_connect.send_config_set(config_commands)
+       print (output)
+
