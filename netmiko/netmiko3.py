8b18cc9d39d253885ee09b0924211f24aa1f259d
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-14 13:51:17 +0900

    add netmiko3 commit

---
+++ netmiko/netmiko3.py
@@ -0,0 +1,15 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+router1 = {
+    "device_type": "cisco_ios",
+    "host": "10.1.1.3",
+    "username": "ccnp",
+    "password": getpass(),
+}
+
+# Will automatically 'disconnect()'
+with ConnectHandler(**router1) as net_connect:
+    print(net_connect.find_prompt())
+    output = net_connect.send_command("show ip int brief")
+    print(output)
