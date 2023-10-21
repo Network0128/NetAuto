0ddcdc992b2d237fcb506b5fd388ae83a6ec86ce
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-21 15:03:43 +0900

    Update na4.py

--- NAPALM/na4.py
+++ NAPALM/na4.py
@@ -7,7 +7,7 @@ switch1 = driver('10.1.1.11', 'ccnp', 'cisco')
 switch1.open()
 
 ip_address = socket.gethostbyname('google.com')
-output = switch1.ping('ip_address')
+output = switch1.ping(ip_address) #주의
 
 print(json.dumps(output,indent=4))
 switch1.close()
