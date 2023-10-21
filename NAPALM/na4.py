666aa1af8c38bd2e251221606763a33333292abf
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-21 14:58:36 +0900

    add napalm commit

---
+++ NAPALM/na4.py
@@ -0,0 +1,13 @@
+import socket
+import json
+from napalm import get_network_driver
+
+driver = get_network_driver('ios')
+switch1 = driver('10.1.1.11', 'ccnp', 'cisco')
+switch1.open()
+
+ip_address = socket.gethostbyname('google.com')
+output = switch1.ping('ip_address')
+
+print(json.dumps(output,indent=4))
+switch1.close()
