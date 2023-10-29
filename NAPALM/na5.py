8db006b9ff58086d3de196dd4d363d00dde59293
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-30 00:19:39 +0900

    Update na5.py

--- NAPALM/na5.py
+++ NAPALM/na5.py
@@ -1,7 +1,7 @@
 from napalm import get_network_driver
-driver = get_network_driver('ios')
-switch1 = driver('10.1.1.11', 'ccnp', 'cisco')
-switch1.open()
+driver = get_network_driver('ios') # 'ios' : Cisco IOS 장비 사용
+switch1 = driver('10.1.1.11', 'ccnp', 'cisco') # IP 주소와 인증 정보로 연결 객체 생성
+switch1.open() # 장비 연결
 
 print('Accessing 10.1.1.11')
 
@@ -10,4 +10,4 @@ switch1.load_merge_candidate(filename='ACL1.cfg') #새로운 설정 로드
 print(switch1.compare_config()) # 변경 사항 확인
 switch1.commit_config() # 변경 사항 커밋(적용)
 
-switch1.close()
+switch1.close() # 연결 종료
