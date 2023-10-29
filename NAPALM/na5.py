be4f1d6dc535c977a65296dff702c7b3789caadb
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-29 16:36:46 +0900

    add napalm commit

--- NAPALM/na5.py
+++ NAPALM/na5.py
@@ -1,24 +1,9 @@
 from napalm import get_network_driver
-
 driver = get_network_driver('ios')
-router = driver('10.1.1.3', 'ccnp', 'cisco')
-router.open()
+iosvl2 = driver('10.1.1.11', 'david', 'cisco')
+iosvl2.open()
 
-print('Accessing R1 : 10.1.1.3')
-
-cmds = [
-    'interface l0',
-    'ip add 1.1.1.1 255.255.255.0'
-]
-
-config_str = '\n'.join(cmds)
-
-router.load_merge_candidate(config=config_str)
-
-router.commit_config()
-
-output = router.get_config()
-
-print(output)
-
-router.close()
+print ('Accessing 10.1.1.11')
+iosvl2.load_merge_candidate(filename='ACL1.cfg')
+iosvl2.commit_config()
+iosvl2.close()
