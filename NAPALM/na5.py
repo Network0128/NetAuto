bb8f4f4296fac6be18856782788bd32ee17440d1
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-30 08:48:21 +0900

    Update na5.py

--- NAPALM/na5.py
+++ NAPALM/na5.py
@@ -5,7 +5,7 @@ switch1.open() # 장비 연결
 
 print('Accessing 10.1.1.11')
 
-switch1.load_merge_candidate(filename='ACL1.cfg') #새로운 설정 로드
+switch1.load_merge_candidate(filename='Merge1.cfg') #새로운 설정 로드
 
 print(switch1.compare_config()) # 변경 사항 확인
 switch1.commit_config() # 변경 사항 커밋(적용)
