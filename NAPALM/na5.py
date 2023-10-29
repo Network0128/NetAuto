d85b35876e45bbfbc98b69096f5c75d6e34ffab5
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-29 15:51:55 +0900

    add napalm commit

---
+++ NAPALM/na5.py
@@ -0,0 +1,23 @@
+from napalm import get_network_driver
+
+driver = get_network_driver('ios')
+router = driver('10.1.1.3', 'ccnp', 'cisco')
+router.open()
+
+print('Accessing R1 : 10.1.1.3')
+
+cmds = [
+    'interface l0',
+    'ip add 1.1.1.1 255.255.255.0'
+]
+
+router.load_merge_candidate(config=cmds)
+
+router.commit_config()
+
+output = router.get_config()
+
+print(output)
+
+router.close()
+
