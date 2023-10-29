ae6a352244d83558c99c95225f2a81fc2517eb66
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-29 16:37:43 +0900

    add napalm commit

--- NAPALM/na5.py
+++ NAPALM/na5.py
@@ -3,7 +3,7 @@ driver = get_network_driver('ios')
 iosvl2 = driver('10.1.1.11', 'david', 'cisco')
 iosvl2.open()
 
-print ('Accessing 10.1.1.11')
+print('Accessing 10.1.1.11')
 iosvl2.load_merge_candidate(filename='ACL1.cfg')
 iosvl2.commit_config()
 iosvl2.close()
