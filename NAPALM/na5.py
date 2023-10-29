6f3a1c926d82d5d0a393f163eeef8f9dc64c5824
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-30 00:18:08 +0900

    Update na5.py

--- NAPALM/na5.py
+++ NAPALM/na5.py
@@ -4,6 +4,10 @@ switch1 = driver('10.1.1.11', 'ccnp', 'cisco')
 switch1.open()
 
 print('Accessing 10.1.1.11')
-switch1.load_merge_candidate(filename='ACL1.cfg')
-switch1.commit_config()
+
+switch1.load_merge_candidate(filename='ACL1.cfg') #새로운 설정 로드
+
+print(switch1.compare_config()) # 변경 사항 확인
+switch1.commit_config() # 변경 사항 커밋(적용)
+
 switch1.close()
