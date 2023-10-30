ee06bd219ea551d04a0931e1685eca4ab4aa249c
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-10-30 11:53:21 +0900

    add napalm commit

--- NAPALM/na6.py
+++ NAPALM/na6.py
@@ -5,9 +5,10 @@ router1 = driver('10.1.1.3', 'ccnp', 'cisco')
 router1.open()
 
 print('Accessing 10.1.1.3')
-router1.load_merge_candidate(filename='Merge1.cfg')
+router1.load_merge_candidate(filename='Merge1.cfg') #새로운 설정 로드(병합)
 
-diffs = router1.compare_config()
+diffs = router1.compare_config() # 변경 사항 확인, 현재 설정과 새로 로드된 설정의 차이점 확인
+#만약 변경사항(diffs)이 있다면, 변경사항 commit(적용), 그렇지 않으면 변경사항 폐기(discard)
 if len(diffs) > 0:
     print(diffs)
     router1.commit_config()
