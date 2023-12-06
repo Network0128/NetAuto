6171488ffe5118397daeac49e382d63b7e9cd66e
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-12-06 16:56:50 +0900

    add ansible commit

---
+++ 01.telnet/telnet1.py
@@ -0,0 +1,24 @@
+#UbuntuDocker → R1
+import getpass
+import telnetlib
+
+HOST = "10.1.1.21"
+user = input("Enter your remote account: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"sh ip int br\n")
+tn.write(b"conf t\n")
+tn.write(b"int l0\n")
+tn.write(b"ip add 1.1.1.1 255.255.255.0\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
---
+++ 01.telnet/telnet2.py
@@ -0,0 +1,31 @@
+#UbuntuDocker → S1
+import getpass
+import telnetlib
+
+HOST = "10.1.1.11"
+user = input("Enter your remote account: ")
+
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"conf t\n")
+tn.write(b"vlan 2\n")
+tn.write(b"name Python_VLAN_2\n")
+tn.write(b"vlan 3\n")
+tn.write(b"name Python_VLAN_3\n")
+tn.write(b"vlan 4\n")
+tn.write(b"name Python_VLAN_4\n")
+tn.write(b"vlan 5\n")
+tn.write(b"name Python_VLAN_5\n")
+tn.write(b"end\n")
+tn.write(b"sh vlan br\n")
+tn.write(b"wr\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
