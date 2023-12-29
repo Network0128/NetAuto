13dbdd319e380d79edbad5b2c2b03c0e4097cc13
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-12-29 14:16:00 +0900

    add ansible commit

--- 01.telnet/telnet1.py
+++ 01.telnet/telnet1.py
@@ -19,6 +19,7 @@ tn.write(b"conf t\n")
 tn.write(b"int l0\n")
 tn.write(b"ip add 1.1.1.1 255.255.255.0\n")
 tn.write(b"end\n")
+tn.write(b"sh ip int br\n")
 tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
