6aaa8bf48ce5772d88a3aa49973d963d1d5a6768
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-12-31 11:08:15 +0900

    Create telent6.py

---
+++ 01.telnet/telent6.py
@@ -0,0 +1,31 @@
+#telnet5.py의 가독성을 높이기 위해 수정합니다.
+import getpass
+import telnetlib
+
+# 사용자 이름과 비밀번호를 입력받습니다.
+user = input("Enter your telnet username: ")
+password = getpass.getpass("Enter your telnet password: ")
+
+# 'myswitches' 파일을 엽니다.
+with open('myswitches') as f:
+    # 파일의 각 줄에 대해 반복합니다.
+    for IP in f:
+        IP = IP.strip()
+        print(f'Get running Config from Switch {IP}')
+        
+        tn = telnetlib.Telnet(IP)
+        
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
+        
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+        
+        tn.write(b"terminal length 0\nsh run\nexit\n")
+        
+        readoutput = tn.read_all()
+        
+        # 출력을 파일에 저장합니다.
+        with open(f'switch{IP}', 'w') as saveoutput:
+            saveoutput.write(readoutput.decode('ascii'))
+            saveoutput.write('\n')
