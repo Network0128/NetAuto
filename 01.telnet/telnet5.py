df2fb1eb5018a1ec0065f81ade0381bb2e129146
NetLearn <125962055+Network0128@users.noreply.github.com>
2023-12-31 11:02:01 +0900

    Update telnet5.py

--- 01.telnet/telnet5.py
+++ 01.telnet/telnet5.py
@@ -9,19 +9,20 @@ f=open('myswitches')
 for IP in f:
     IP=IP.strip()
     print('Get running Config from Switch ' + IP)
-    HOST = IP
-    tn = telnetlib.Telnet(HOST)
+    tn = telnetlib.Telnet(IP)
+    #사용자 이름 입력
     tn.read_until(b"Username: ")
     tn.write(user.encode('ascii') + b"\n")
+    #비밀 번호 입력
     if password:
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-    tn.write(b"terminal length 0\n")
-    tn.write(b"sh run\n")
-    tn.write(b"exit\n")
-
+    #명령어 연속 실행
+    tn.write(b"terminal length 0\nsh run\nexit\n")
+    #출력을 읽음
     readoutput = tn.read_all()
+    #출력을 파일에 저장
     saveoutput = open('switch' + HOST,'w')
     saveoutput.write(readoutput.decode('ascii'))
     saveoutput.write('\n')
-    saveoutput.close
+    saveoutput.close()
