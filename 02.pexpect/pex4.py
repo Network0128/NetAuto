4a6dee9a6f7ab7bbef2dd9d9809d9dac966d12f3
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-01-06 22:05:48 +0900

    Create pex4.py

---
+++ 02.pexpect/pex4.py
@@ -0,0 +1,19 @@
+#Python의 pexpect 모듈을 사용하여 SSH로 특정 호스트에 접속하고, 'sh ip int br' 커맨드를 실행한 후 그 결과를 출력하고, 세션을 종료하는 것을 자동화
+
+import pexpect  # pexpect 라이브러리 임포트
+
+ssh_session = pexpect.spawn("ssh ccnp@10.1.1.3")  # SSH 세션을 위한 pexpect 생성 객체
+
+ssh_session.expect("Password:")  # 비밀번호 프롬프트 대기
+
+ssh_session.sendline("cisco")  # 비밀번호 전송
+
+ssh_session.expect("R1#")  # 쉘 프롬프트 대기
+
+ssh_session.sendline("sh ip int br")  # 커맨드 전송
+
+ssh_session.expect("R1#")  # 커맨드 출력 대기
+
+print(ssh_session.before.decode())  # 출력 결과 디코딩 후 출력
+
+ssh_session.close()  # SSH 세션 종료
