aa9beccbe43bc6933cde4164f47180bfc9c354c2
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-02-04 22:34:53 +0900

    Update 06_Configuration_changes.py

--- 04.netmiko/06_Configuration_changes.py
+++ 04.netmiko/06_Configuration_changes.py
@@ -1,3 +1,6 @@
+# Cisco IOS 장비의 설정을 변경하고 저장하는 과정을 자동화합니다.
+# send_config_set과 save_config 메소드를 통해 설정 파일을 변경하고, 저장합니다.
+
 from netmiko import ConnectHandler
 from getpass import getpass
 
@@ -8,7 +11,7 @@ router1 = {
     "password": getpass(),
 }
 
-commands = ["logging buffered 100000"]
+commands = ["logging buffered 100000"] #라우터의 내부 로깅 버퍼 크기를 100,000바이트로 설정
 with ConnectHandler(**router1) as net_connect:
     output = net_connect.send_config_set(commands) #send_config_set: 전역 설정 모드의 명령어를 보내는데 사용
     output += net_connect.save_config() #네트워크 장비의 현재 설정 저장
@@ -16,3 +19,4 @@ with ConnectHandler(**router1) as net_connect:
 print()
 print(output)
 print()
+#print(f"\n{output}\n")
