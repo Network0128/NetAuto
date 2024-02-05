4baaed0fb7084363dec8f8893515f0a24195332a
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-02-05 15:49:11 +0900

    Update 04_Connecting_to_multiple_devices.py

--- 04.netmiko/04_Connecting_to_multiple_devices.py
+++ 04.netmiko/04_Connecting_to_multiple_devices.py
@@ -36,8 +36,7 @@ switch3 = {
 command="show ip int brief"
 
 for device in (router1, switch1, switch2, switch3):
-    net_connect = ConnectHandler(**device)
-    print(net_connect.find_prompt())
-    output = net_connect.send_command(command)
-    print(output)
-    net_connect.disconnect()
+    with ConnectHandler(**device) as net_connect:
+        print(net_connect.find_prompt) #생략가능
+        output = net_connect.send_command(command)
+        print(output)
