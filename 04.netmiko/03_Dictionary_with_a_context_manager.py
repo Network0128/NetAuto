725d8c1af14fcf37830846faaba55e04638ef6f8
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-02-05 15:02:58 +0900

    Update 03_Dictionary_with_a_context_manager.py

--- 04.netmiko/03_Dictionary_with_a_context_manager.py
+++ 04.netmiko/03_Dictionary_with_a_context_manager.py
@@ -8,8 +8,8 @@ router1 = {
     "password": getpass(),
 }
 
-# Will automatically 'disconnect()'
-with ConnectHandler(**router1) as net_connect:
-    print(net_connect.find_prompt())
-    output = net_connect.send_command("show ip int brief")
-    print(output)
+# Will automatically 'disconnect()' -> 'ConnectHandler'를 컨텍스트 매니저로 사용하여 자동 연결 해제를 보장
+with ConnectHandler(**router1) as net_connect:  
+    print(net_connect.find_prompt()) # 장비의 명령 프롬프트를 출력하여 연결 확인
+    output = net_connect.send_command("show ip int brief")  # 장비에 명령을 보내고 출력을 저장
+    print(output)  # 명령의 출력을 출력
