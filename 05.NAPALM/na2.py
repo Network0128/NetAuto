3ce9d1e1cd20b0479b1dbd5a49b20cd1a1e2b009
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-02-06 21:43:59 +0900

    Update na2.py

--- 05.NAPALM/na2.py
+++ 05.NAPALM/na2.py
@@ -1,13 +1,16 @@
-from napalm import get_network_driver
+#NAPALM 라이브러리를 사용하여 특정 네트워크 장치(‘ios’)에 연결하고, 해당 장치의 모든 인터페이스 정보를 가져와 출력하는 과정을 수행
 
-driver = get_network_driver('ios')
+from napalm import get_network_driver  # napalm 라이브러리에서 get_network_driver 함수 가져오기
 
-device = driver(hostname='10.1.1.3', username='ccnp', password='cisco')
+driver = get_network_driver('ios')  # 'ios' 드라이버 가져오기
 
-device.open()
+device = driver(hostname='10.1.1.3', username='ccnp', password='cisco')  # 장치 연결 정보 설정
 
-interfaces = device.get_interfaces()
+device.open()  # 장치에 연결 시작
 
-print(interfaces)
+interfaces = device.get_interfaces()  # 모든 인터페이스 정보 가져오기
 
-device.close()
+print(interfaces)  # 인터페이스 정보 출력
+
+device.close()  # 장치 연결 종료
+
