965ace5621e15c113e4513893d43a7320b3642c1
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-02-06 21:38:55 +0900

    Update na1.py

--- 05.NAPALM/na1.py
+++ 05.NAPALM/na1.py
@@ -3,14 +3,15 @@
 
 import napalm  # napalm 라이브러리 가져오기
 
+# 드라이버 설정 : 접속하는 장비의 유형('ios', 'nxos', 'junos' 등)
 driver = napalm.get_network_driver('ios')  # 'ios' 드라이버 가져오기
-
+# 장비에 연결
 device = driver(hostname='10.1.1.21', username='ccnp', password='cisco')  # 장치 연결 정보 설정
-
 device.open()  # 장치에 연결 시작
 
+#get_facts() 메서드 : 장비 정보 가져오기(장비 모델, 시리얼 번호, 운영 체제 버전 등)
 output = device.get_facts()  # 장치로부터 사실 가져오기
 
-print(output)  # 출력 인쇄
+print(output)  # 화면에 출력
 
 device.close()  # 장치 연결 닫기
