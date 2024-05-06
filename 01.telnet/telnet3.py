b0c03c84c1d35c891a0ae2063bfaee3e33a31323
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-05-07 08:46:54 +0900

    Update telnet3.py

--- 01.telnet/telnet3.py
+++ 01.telnet/telnet3.py
@@ -1,14 +1,14 @@
 #Ubuntu To SW1 : Using loop
 #반복문을 활용하여 VLAN을 생성하는 파이썬 자동화 코드를 완성하시오.
-import getpass
 import telnetlib
 
 HOST = "10.1.1.11"
-user = input("Enter your telnet username: ")
-password = getpass.getpass("Enter your telnet password: ")
+user = "ccnp"  # 사전에 정의된 사용자 이름
+password = "cisco"  # 사전에 정의된 비밀번호
 
-tn = telnetlib.Telnet(HOST)
+tn = telnetlib.Telnet(HOST) # 지정된 호스트로 텔넷 연결 시작
 
+# 사용자 이름과 비밀번호를 자동으로 전송
 tn.read_until(b"Username: ")
 tn.write(user.encode('ascii') + b"\n")
 if password:
@@ -22,7 +22,6 @@ for n in range(2, 11):  # 2부터 10까지 반복
 
 tn.write(b"end\n")
 tn.write(b"sh vl br\n")
-tn.write(b"wr\n")
 tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
