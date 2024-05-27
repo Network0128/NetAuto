a8c2cd0cc49e32d488aee548b2d97eb07e05e7ec
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-05-27 23:07:00 +0900

    Update 02_Connect_using_a_dictionary.py

--- 04.netmiko/02_Connect_using_a_dictionary.py
+++ 04.netmiko/02_Connect_using_a_dictionary.py
@@ -1,16 +1,15 @@
 from netmiko import ConnectHandler
-from getpass import getpass
 
 router1 = {
     "device_type": "cisco_ios",
     "host": "10.1.1.21",
     "username": "ccnp",
-    "password": getpass(),
+    "password": "cisco",
 }
 
 net_connect = ConnectHandler(**router1) # **router1 : 딕셔너리의 각 키-값 쌍이 함수의 인자로 전달
-output = net_connect.send_command("show ip int brief")
-print(output)
-output = net_connect.send_command("show run")
-print(output)
+
+print(net_connect.send_command("show ip int brief")
+print(net_connect.send_command("show run")
+
 net_connect.disconnect()
