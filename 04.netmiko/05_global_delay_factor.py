0a1b353a2075fdc5f76c9d50b5a47b1f6a7ef8b2
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-05-27 23:28:03 +0900

    Update 05_global_delay_factor.py

--- 04.netmiko/05_global_delay_factor.py
+++ 04.netmiko/05_global_delay_factor.py
@@ -2,19 +2,16 @@
 # 다양한 네트워크 환경에서 느린 장비의 응답을 관리하기 위해 "global_delay_factor": 2 를 사용합니다.
 
 from netmiko import ConnectHandler
-from getpass import getpass
 
 router1 = {
     "device_type": "cisco_ios",
     "host": "10.1.1.21",
     "username": "ccnp",
-    "password": getpass(),
+    "password": "cisco",
     "global_delay_factor": 2, #﻿모든 지연 시간을 2배로 증가시킨다.
 }
 
 command = "show ip arp"
 net_connect = ConnectHandler(**router1)
-output = net_connect.send_command(command)
+pirnt(net_connect.send_command(command))
 net_connect.disconnect()
-
-print(f"\n{output}\n")
