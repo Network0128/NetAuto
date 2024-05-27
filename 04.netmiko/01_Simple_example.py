4f0a4672b90e4dd044c7fcc571f096763ca4c065
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-05-27 23:01:18 +0900

    Update 01_Simple_example.py

--- 04.netmiko/01_Simple_example.py
+++ 04.netmiko/01_Simple_example.py
@@ -1,16 +1,14 @@
 from netmiko import ConnectHandler 
-from getpass import getpass 
 
 net_connect = ConnectHandler(
     device_type="cisco_ios",
     host="10.1.1.21",
     username="ccnp",
-    password=getpass()
+    password="cisco"
 )
 
-output = net_connect.send_command("show ip int brief") #send_command: 관리자 모드의 명령어를 보내는데 사용
-print(output)
-output = net_connect.send_command("show run") 
-print(output)
+#send_command: 관리자 모드의 명령어를 보내는데 사용
+print(net_connect.send_command("show ip int brief"))
+print(net_connect.send_command("show run"))
 
 net_connect.disconnect()
