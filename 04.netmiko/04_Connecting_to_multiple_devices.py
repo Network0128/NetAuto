f0fc15b8b298cf8a538b577a5f9a7536fc422b3f
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-05-27 23:25:37 +0900

    Update 04_Connecting_to_multiple_devices.py

--- 04.netmiko/04_Connecting_to_multiple_devices.py
+++ 04.netmiko/04_Connecting_to_multiple_devices.py
@@ -1,42 +1,37 @@
 #Netmiko를 사용하여 여러 Cisco 장비에 대해 "show ip int brief" 명령을 실행합니다.
 
 from netmiko import ConnectHandler
-from getpass import getpass
-
-password = getpass()
 
 router1 = {
     "device_type": "cisco_ios",
     "host": "10.1.1.21",
     "username": "ccnp",
-    "password": password,
+    "password": "cisco",
 }
 
 switch1 = {
     "device_type": "cisco_ios",
     "host": "10.1.1.11",
     "username": "ccnp",
-    "password": password,
+    "password": "cisco",
 }
 
 switch2 = {
     "device_type": "cisco_ios",
     "host": "10.1.1.12",
     "username": "ccnp",
-    "password": password,
+    "password": "cisco",
 }
 
 switch3 = {
     "device_type": "cisco_ios",
     "host": "10.1.1.13",
     "username": "ccnp",
-    "password": password,
+    "password": "cisco",
 }
 
 command="show ip int brief"
 
 for device in (router1, switch1, switch2, switch3):
     with ConnectHandler(**device) as net_connect:
-        print(net_connect.find_prompt) #생략가능
-        output = net_connect.send_command(command)
-        print(output)
+        print(net_connect.send_command(command))
