6818ed967b7f79e491a6e1a244817c36eba6e07a
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-06-02 22:21:36 +0900

    Update na5.py

--- 05.NAPALM/na5.py
+++ 05.NAPALM/na5.py
@@ -1,8 +1,9 @@
-#NAPALM 라이브러리를 사용하여 Cisco IOS 스위치에 연결하고, 'Merge1.cfg' 파일에 정의된 새로운 설정을 현재 구성과 병합하는 과정을 구현합니다. 
-#병합 후 변경 사항이 있는지 확인하고, 변경 사항이 있으면 이를 커밋(적용)한 다음 연결을 종료합니다. 
-#필요에 따라 특정 파일 시스템을 대상으로 하는 선택적 인자를 설정하는 방법도 주석으로 언급되어 있습니다.
+NAPALM 라이브러리를 사용하여 Cisco IOS 스위치에 연결하고, 
+'Merge1.cfg' 파일에 정의된 새로운 설정을 현재 구성과 병합하는 과정을 구현합니다. 
+병합 후 변경 사항이 있는지 확인하고, 변경 사항이 있으면 이를 커밋(적용)한 다음 연결을 종료합니다. 
+필요에 따라 특정 파일 시스템을 대상으로 하는 선택적 인자를 설정하는 방법도 주석으로 언급되어 있습니다.
 
-from napalm import get_network_driver  # napalm 라이브러리로부터 get_network_driver 함수를 임포트합니다. 네트워크 장비 관리에 사용됩니다.
+from napalm import get_network_driver  # napalm 라이브러리로부터 get_network_driver 함수를 임포트합니다.
 driver = get_network_driver('ios')  # 'ios'를 지정하여 Cisco IOS 장비를 위한 네트워크 드라이버를 가져옵니다.
 switch1 = driver('10.1.1.11', 'ccnp', 'cisco')  # 드라이버를 사용하여 스위치에 대한 연결 객체를 생성합니다. IP 주소와 인증 정보가 포함됩니다.
 # 필요에 따라 위에 다음을 추가: ​​optional_args={'dest_file_system': 'flash0:'}  # 명령어 실행 결과를 특정 파일 시스템에 저장하기 위한 선택적 인자를 설정할 수 있습니다.
@@ -18,8 +19,9 @@ switch1.commit_config()  # 변경 사항을 커밋(적용)합니다.
 
 switch1.close()  # 장비와의 연결을 종료합니다.
 
-#Cisco IOS 장치에서 ip scp server enable 명령은 SCP(Secure Copy Protocol) 서버를 활성화하는 데 사용됩니다.
-#장치와의 안전한 파일 전송을 위해 SCP를 사용하려는 경우 이 명령이 필요합니다. SCP는 데이터 전송을 위해 SSH(Secure Shell)를 사용하여 인증과 암호화를 모두 제공합니다.
-#NAPALM과 함께 사용하는 방법을 사용하는 경우 Cisco IOS 장치에서 SCP 서버를 활성화해야 합니다.
+Cisco IOS 장치에서 ip scp server enable 명령은 SCP(Secure Copy Protocol) 서버를 활성화하는데 사용됩니다.
+장치와의 안전한 파일 전송을 위해 SCP를 사용하려는 경우 이 명령이 필요합니다. 
+SCP는 데이터 전송을 위해 SSH(Secure Shell)를 사용하여 인증과 암호화를 모두 제공합니다.
+NAPALM과 함께 사용하는 방법을 사용하는 경우 Cisco IOS 장치에서 SCP 서버를 활성화해야 합니다.
 
 
