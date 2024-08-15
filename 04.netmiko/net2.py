76ed5bbe6cebf280f723b1ca7066bb2423cfdd79
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-08-16 08:49:14 +0900

    Update net2.py

--- 04.netmiko/net2.py
+++ 04.netmiko/net2.py
@@ -34,4 +34,4 @@ for device in devices:  # 각 스위치에 대해 반복
         print("Creating VLAN " + str(n))  # 생성 중인 VLAN 번호 출력
         config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]  # VLAN 생성 및 이름 지정 명령어
         output = net_connect.send_config_set(config_commands)  # 명령어 실행 및 출력
-        print(output)  # 실행 결과 출력
+        print(output)  # 결과 출력
