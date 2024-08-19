8f74a4389d25a2315fd781b28e658501d8709860
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-08-19 11:44:38 +0900

    Update 06_Configuration_changes.py

--- 04.netmiko/06_Configuration_changes.py
+++ 04.netmiko/06_Configuration_changes.py
@@ -20,4 +20,45 @@ with ConnectHandler(**router1) as net_connect:
 print()
 print(output)
 print()
+--------------------------------------------------------------
+from netmiko import ConnectHandler
 
+router1 = {
+    "device_type": "cisco_ios",
+    "host": "10.1.1.21",
+    "username": "ccnp",
+    "password": "cisco",
+}
+
+switch1 = {
+    "device_type": "cisco_ios",
+    "host": "10.1.1.11",
+    "username": "ccnp",
+    "password": "cisco",
+}
+
+switch2 = {
+    "device_type": "cisco_ios",
+    "host": "10.1.1.12",
+    "username": "ccnp",
+    "password": "cisco",
+}
+
+switch3 = {
+    "device_type": "cisco_ios",
+    "host": "10.1.1.13",
+    "username": "ccnp",
+    "password": "cisco",
+}
+
+#command="show ip int brief"
+commands = ["interface loopback 0", "ip address 1.1.1.1 255.255.255.0"]
+
+for device in (router1, switch1, switch2, switch3):
+    with ConnectHandler(**device) as net_connect:
+        print(net_connect.find_prompt())
+        output=net_connect.send_config_set(commands)
+        output += net_connect.save_config()
+        #output=net_connect.send_command(command)        
+        print(output)
+        print()
