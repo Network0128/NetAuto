8fcb461398128f9e8fa6b9b5472e1fbb4e45f27c
NetLearn <125962055+Network0128@users.noreply.github.com>
2024-11-24 21:49:03 +0900

    Update 01_Simple_example.py

--- 04.netmiko/01_Simple_example.py
+++ 04.netmiko/01_Simple_example.py
@@ -1,14 +1,16 @@
-from netmiko import ConnectHandler 
+from netmiko import ConnectHandler  # Netmiko 라이브러리를 사용하여 네트워크 장비와 SSH 연결
 
+# 네트워크 장비 연결 설정
 net_connect = ConnectHandler(
-    device_type="cisco_ios",
-    host="10.1.1.21",
-    username="ccnp",
-    password="cisco"
+    device_type="cisco_ios",  # Cisco IOS 장비 타입 설정
+    host="10.1.1.21",        # 장비의 IP 주소
+    username="ccnp",         # 장비 접속 사용자 이름
+    password="cisco"         # 장비 접속 비밀번호
 )
 
-#send_command: 관리자 모드의 명령어를 보내는데 사용
-print(net_connect.send_command("show ip int brief"))
-print(net_connect.send_command("show run"))
+# send_command: 관리자 모드의 명령어를 보내는데 사용
+print(net_connect.send_command("show ip int brief"))  # 인터페이스 상태 요약 출력
+print(net_connect.send_command("show run"))          # 실행 중인 설정 출력
 
-net_connect.disconnect()
+net_connect.disconnect()  # 연결 종료
+
