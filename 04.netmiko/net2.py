d82beb95c9d7be0224681527ab1440232f3fa5c1
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-02-03 22:51:08 +0900

    Update net2.py

--- 04.netmiko/net2.py
+++ 04.netmiko/net2.py
@@ -1,32 +1,29 @@
 # Netmiko 라이브러리를 사용하여 3개의 Cisco 스위치에 연결하고 VLAN 2부터 10까지 생성
 
-from netmiko import ConnectHandler  # Netmiko 라이브러리를 임포트하여 네트워크 장비 자동화
+from netmiko import ConnectHandler  
 
-# 첫 번째 스위치 연결 정보
 switch1 = {
-    'device_type': 'cisco_ios',  # 디바이스 타입으로 cisco_ios 지정
-    'ip': '10.1.1.11',  # 스위치의 IP 주소
-    'username': 'ccnp',  # 접속을 위한 사용자 이름
-    'password': 'cisco'  # 접속을 위한 비밀번호
+    'device_type': 'cisco_ios',
+    'ip': '10.1.1.11',  
+    'username': 'ccnp', 
+    'password': 'cisco'  
 }
 
-# 두 번째 스위치 연결 정보
 switch2 = {
-    'device_type': 'cisco_ios',  # 디바이스 타입으로 cisco_ios 지정
-    'ip': '10.1.1.12',  # 스위치의 IP 주소
-    'username': 'ccnp',  # 접속을 위한 사용자 이름
-    'password': 'cisco'  # 접속을 위한 비밀번호
+    'device_type': 'cisco_ios', 
+    'ip': '10.1.1.12',  
+    'username': 'ccnp', 
+    'password': 'cisco' 
 }
 
-# 세 번째 스위치 연결 정보
 switch3 = {
-    'device_type': 'cisco_ios',  # 디바이스 타입으로 cisco_ios 지정
-    'ip': '10.1.1.13',  # 스위치의 IP 주소
-    'username': 'ccnp',  # 접속을 위한 사용자 이름
-    'password': 'cisco'  # 접속을 위한 비밀번호
+    'device_type': 'cisco_ios',  
+    'ip': '10.1.1.13',  
+    'username': 'ccnp', 
+    'password': 'cisco' 
 }
 
-devices = [switch1, switch2, switch3]  # 연결할 스위치 목록
+devices = [switch1, switch2, switch3]  
 
 for device in devices:  # 각 스위치에 대해 반복
     net_connect = ConnectHandler(**device)  # 스위치에 연결
