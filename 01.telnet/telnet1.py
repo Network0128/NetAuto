ffdcdac62a27d19a75d91c9b8eac9e60f721653d
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-02-05 23:33:30 +0900

    Update telnet1.py

--- 01.telnet/telnet1.py
+++ 01.telnet/telnet1.py
@@ -28,20 +28,20 @@ tn.write(b"exit\n") # 텔넷 세션 종료
 print(tn.read_all().decode('ascii')) # 텔넷 세션 출력 읽고 디코드하여 출력
 
 # 바이트 타입 데이터: 주로 파일 입출력, 네트워크 통신, 바이너리 데이터 처리 등에서 사용된다.
-# ASCII 인코딩: string 타입의 데이터를 byte 타입으로 변환할 때 사용되는 인코딩 방식 중 하나다. (하지만 다른 인코딩 방식도 존재, 예: UTF-8)
+# ASCII,UTF-8 인코딩: string 타입의 데이터를 byte 타입으로 변환할 때 사용되는 인코딩 방식 중 하나다.
 # string(유니코드) <-> byte(ASCII/UTF-8) <-> string(유니코드)
 # ASCII나 UTF-8 같은 인코딩 방식을 사용하여 문자열을 바이트로 변환하고, 다시 문자열로 복원할 수 있다.
 
 # ASCII 인코딩/디코딩
 s = "hello"
-b = s.encode('ascii')  # string -> byte (ASCII 인코딩)
+b = s.encode('ascii')       # string -> byte (ASCII 인코딩)
 print(b)  # 출력: b'hello'
 s_back = b.decode('ascii')  # byte -> string (ASCII 디코딩)
 print(s_back)  # hello
 
 # UTF-8 인코딩/디코딩
 s_utf8 = "안녕하세요"
-b_utf8 = s_utf8.encode('utf-8')  # string -> byte (UTF-8 인코딩)
+b_utf8 = s_utf8.encode('utf-8')        # string -> byte (UTF-8 인코딩)
 print(b_utf8)  # 출력: b'\xec\x95\x88\xeb\x85\x95\xed\x95\x98\xec\x84\xb8\xec\x9a\x94'
-s_back_utf8 = b_utf8.decode('utf-8')  # byte -> string (UTF-8 디코딩)
+s_back_utf8 = b_utf8.decode('utf-8')   # byte -> string (UTF-8 디코딩)
 print(s_back_utf8)  # 출력: 안녕하세요
