5918ddd1c8b439d0307eceb859e9e766d8fe1118
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-02-06 22:26:42 +0900

    Update telnet5_with.py

--- 01.telnet/telnet5_with.py
+++ 01.telnet/telnet5_with.py
@@ -2,22 +2,21 @@
 #가독성을 높이기 위해 telnet5.py를 수정(with문 사용)
 import telnetlib
 
-user = "ccnp"
-password = "cisco"
+user, password = "ccnp", "cisco"
 
-# 'all_devices' 파일 오픈
+# 'all_devices' 파일을 열어 IP 목록을 읽음
 with open('all_devices') as f:
     for IP in f:
         IP = IP.strip()
         print(f'Get running Config from Device {IP}')
-        tn = telnetlib.Telnet(IP)
-        tn.read_until(b"Username: ")
-        tn.write(user.encode('ascii') + b"\n")
-        tn.read_until(b"Password: ")
-        tn.write(password.encode('ascii') + b"\n")
-        tn.write(b"terminal length 0\nsh run\nexit\n")
-        read_output = tn.read_all()
-        
-        # 출력을 파일에 저장
-        with open(f'device_{IP}', 'w') as save_output:
-            save_output.write(read_output.decode('ascii'))           
+        # Telnet 연결 및 명령어 실행
+        with telnetlib.Telnet(IP) as tn: #tn.close()를 명시적으로 호출할 필요가 없이, 연결이 끝나면 자동으로 닫힘
+            tn.read_until(b"Username: ")
+            tn.write(user.encode('ascii') + b"\n")
+            tn.read_until(b"Password: ")
+            tn.write(password.encode('ascii') + b"\n")
+            tn.write(b"terminal length 0\nsh run\nexit\n")
+    
+            # 출력을 파일에 저장
+            with open(f'device_{IP}', 'w') as save_output:
+                save_output.write(tn.read_all().decode('ascii'))           
