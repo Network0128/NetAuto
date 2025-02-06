f7a5fbb5406bfed7188e20fb1a8e5224f8b7e426
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-02-06 22:13:47 +0900

    Update telnet5_with.py

--- 01.telnet/telnet5_with.py
+++ 01.telnet/telnet5_with.py
@@ -7,25 +7,17 @@ password = "cisco"
 
 # 'all_devices' 파일 오픈
 with open('all_devices') as f:
-    # 파일의 각 줄을 아래와 같이 반복
     for IP in f:
         IP = IP.strip()
-        # f-string 문자열 포매팅 방식 : 문자열 내에 중괄호 {}를 사용해 변수나 표현식을 직접 삽입
-        print(f'Get running Config from Switch {IP}')
-        
+        print(f'Get running Config from Device {IP}')
         tn = telnetlib.Telnet(IP)
-        
         tn.read_until(b"Username: ")
         tn.write(user.encode('ascii') + b"\n")
-        
         tn.read_until(b"Password: ")
         tn.write(password.encode('ascii') + b"\n")
-        
         tn.write(b"terminal length 0\nsh run\nexit\n")
-        
         read_output = tn.read_all()
         
         # 출력을 파일에 저장
-        with open(f'device{IP}', 'w') as save_output:
-            save_output.write(read_output.decode('ascii'))
-            save_output.write('\n')
+        with open(f'device_{IP}', 'w') as save_output:
+            save_output.write(read_output.decode('ascii'))           
