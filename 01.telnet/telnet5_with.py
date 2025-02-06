deef79b22eff41f8ba907a8b7d180a7d31ecd7c9
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-02-06 22:32:48 +0900

    Update telnet5_Backup_with.py

--- 01.telnet/telnet5_with.py
+++ 01.telnet/telnet5_with.py
@@ -4,19 +4,18 @@ import telnetlib
 
 user, password = "ccnp", "cisco"
 
-# 'all_devices' 파일을 열어 IP 목록을 읽음
-with open('all_devices') as f:
+with open('all_devices') as f: (with 구문 사용으로 자동 close)
     for IP in f:
         IP = IP.strip()
         print(f'Get running Config from Device {IP}')
-        # Telnet 연결 및 명령어 실행
-        with telnetlib.Telnet(IP) as tn: #tn.close()를 명시적으로 호출할 필요가 없이, 연결이 끝나면 자동으로 닫힘
+        # Telnet 연결 및 명령어 실행 (with 구문 사용으로 자동 close)
+        with telnetlib.Telnet(IP) as tn: 
             tn.read_until(b"Username: ")
             tn.write(user.encode('ascii') + b"\n")
             tn.read_until(b"Password: ")
             tn.write(password.encode('ascii') + b"\n")
             tn.write(b"terminal length 0\nsh run\nexit\n")
     
-            # 출력을 파일에 저장
+            # 출력을 파일에 저장 (with 구문 사용으로 자동 close)
             with open(f'device_{IP}', 'w') as save_output:
                 save_output.write(tn.read_all().decode('ascii'))           
