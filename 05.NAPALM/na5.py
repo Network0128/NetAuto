1165dafdb52ef7d8915a71085d83c3731ae755e0
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-05-28 22:44:13 +0900

    Update na5.py

--- 05.NAPALM/na5.py
+++ 05.NAPALM/na5.py
@@ -5,20 +5,20 @@ NAPALM 라이브러리를 사용하여 Cisco IOS 스위치에 연결하고,
 
 from napalm import get_network_driver  # napalm 라이브러리로부터 get_network_driver 함수를 임포트합니다.
 driver = get_network_driver('ios')  # 'ios'를 지정하여 Cisco IOS 장비를 위한 네트워크 드라이버를 가져옵니다.
-switch1 = driver('10.1.1.11', 'ccnp', 'cisco')  # 드라이버를 사용하여 스위치에 대한 연결 객체를 생성합니다. IP 주소와 인증 정보가 포함됩니다.
+SW1 = driver('10.1.1.11', 'ccnp', 'cisco')  # 드라이버를 사용하여 스위치에 대한 연결 객체를 생성합니다. IP 주소와 인증 정보가 포함됩니다.
 # 필요에 따라 위에 다음을 추가: ​​optional_args={'dest_file_system': 'flash0:'}  # 명령어 실행 결과를 특정 파일 시스템에 저장하기 위한 선택적 인자를 설정할 수 있습니다.
 
-switch1.open()  # 장비에 연결을 시작합니다.
+SW1.open()  # 장비에 연결을 시작합니다.
 
 print('Accessing 10.1.1.11')  # 스위치 접근 시작을 알립니다.
 
-switch1.load_merge_candidate(filename='Merge1.cfg')  # 'Merge1.cfg' 파일에서 새 설정을 불러와 현재 구성과 병합합니다.
+SW1.load_merge_candidate(filename='Merge1.cfg')  # 'Merge1.cfg' 파일에서 새 설정을 불러와 현재 구성과 병합합니다.
 # switch1.load_merge_candidate(filename='/home/ubuntu/PythonHome/4.napalm/Merge1.cfg')  실행이 안될경우 절대경로 사용
 
-print(switch1.compare_config())  # 현재 구성과 새로 불러온 구성 사이의 차이점을 확인합니다.
-switch1.commit_config()  # 변경 사항을 커밋(적용)합니다.
+print(SW1.compare_config())  # 현재 구성과 새로 불러온 구성 사이의 차이점을 확인합니다.
+SW1.commit_config()  # 변경 사항을 커밋(적용)합니다.
 
-switch1.close()  # 장비와의 연결을 종료합니다.
+SW1.close()  # 장비와의 연결을 종료합니다.
 
 Cisco IOS 장치에서 ip scp server enable 명령은 SCP(Secure Copy Protocol) 서버를 활성화하는데 사용됩니다.
 장치와의 안전한 파일 전송을 위해 SCP를 사용하려는 경우 이 명령이 필요합니다. 
