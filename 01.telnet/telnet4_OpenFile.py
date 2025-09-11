324ce5a9876f4b263fbfcfac2163af0be76601db
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-09-11 16:53:06 +0900

    Update telnet4_OpenFile.py

--- 01.telnet/telnet4_OpenFile.py
+++ 01.telnet/telnet4_OpenFile.py
@@ -31,7 +31,32 @@ for IP in f:
     tn.write(b"wr\n")
     tn.write(b"exit\n")
     print(tn.read_all().decode('ascii'))
+------------with as 구문, for 문 사용
+import telnetlib
 
+#HOST = "10.1.1.11"
+user = "ccnp"
+password = "cisco"
+#myswitches=("10.1.1.11","10.1.1.12","10.1.1.13")  # <- 여러대의 스위치 목록들을 파일 단위로 관리
+with open('myswitches') as f:
+    for HOST in f:
+        HOST=HOST.strip()
+        print("Switch's IP add: ",HOST)
+        tn = telnetlib.Telnet(HOST)
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+        tn.write(b"conf t\n")
+        #tn.write(b"no vlan 2-10\n")
+        for i in range(2,11):
+            tn.write(b'vlan '+str(i).encode('ascii')+ b"\n")
+            tn.write(b'name Python_VLAN_'+str(i).encode('ascii')+ b"\n")
+        tn.write(b"end\n")
+        tn.write(b"sh vl br\n")
+        tn.write(b"exit\n")
+        print(tn.read_all().decode('ascii'))
+        
 ----------------------------
 # 파일을 사용하지 않을 경우 : 실습 1 단계
 myswitches = ("10.1.1.11","10.1.1.12","10.1.1.13")  #list, tuple, set, dictionary 모두 가능 주로 불변성의 튜플 사용
