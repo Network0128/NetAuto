00f4d3c47ef6a3c03cfe072d7daf6018531c3df3
NetLearn <125962055+Network0128@users.noreply.github.com>
2025-09-11 17:15:35 +0900

    Update telnet5_Backup.py

--- 01.telnet/telnet5_Backup.py
+++ 01.telnet/telnet5_Backup.py
@@ -19,6 +19,32 @@ for IP in f:
     #명령어 연속 실행
     tn.write(b"terminal length 0\nsh run\nexit\n")
     #출력을 읽어서 파일에 저장
-    save_output = open(f'device_{IP}','w')  # f-string을 사용하여 IP 주소별 파일명 생성, 쓰기 모드로 파일 열기
-    save_output.write(tn.read_all().decode('ascii')) # telnet 세션의 출력(bytes)을 ASCII 문자열로 디코딩하여 파일에 저장
-    save_output.close()
+    save = open(f'device_{IP}','w')  # f-string을 사용하여 IP 주소별 파일명 생성, 쓰기 모드로 파일 열기
+    save.write(tn.read_all().decode('ascii')) # telnet 세션의 출력(bytes)을 ASCII 문자열로 디코딩하여 파일에 저장
+    save.close()
+
+------------
+#스위치만 대상으로 불러오고, 파일로 백업을 할 경우
+
+import telnetlib
+
+#HOST = "10.1.1.11"
+user = "ccnp"
+password = "cisco"
+#myswitches=("10.1.1.11","10.1.1.12","10.1.1.13")  # <- 여러대의 스위치 목록들을 파일 단위로 관리
+with open('myswitches') as f:
+    for HOST in f:
+        HOST=HOST.strip()
+        print("Switch's IP add: ",HOST)
+        tn = telnetlib.Telnet(HOST)
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+        tn.write(b"terminal length 0\n")
+        tn.write(b"sh run\n")
+        tn.write(b"exit\n")
+        save=open(f'switch_{HOST}','w')
+        save.write(tn.read_all().decode('ascii'))
+        save.close
+
