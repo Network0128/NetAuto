a846c2395af27ee6a51e4ac328591b93a02161de
NetLearn <125962055+Network0128@users.noreply.github.com>
2026-01-09 11:52:44 +0900

    Refactor variable name from 'switch' to 'device'

--- 05.NAPALM/na8.py
+++ 05.NAPALM/na8.py
@@ -9,25 +9,25 @@ devices = ['10.1.1.11', '10.1.1.12', '10.1.1.13']  # 장비 IP 주소 리스트
 for IP in devices:  # devices 리스트의 각 IP 주소에 대해 반복 실행
     print(f"Connecting to {IP}")  # 현재 연결 중인 장비 IP 주소 출력
     driver = get_network_driver('ios')  # Cisco IOS 장비를 위한 드라이버 가져오기
-    switch = driver(IP, 'ccnp', 'cisco')  # 인증 정보와 함께 장비에 대한 연결 객체 생성
-    switch.open()  # 장비와의 연결 시작
-    switch.load_merge_candidate(filename='Merge1.cfg')  # 'Merge1.cfg' 파일에서 설정 병합 준비
-    diffs = switch.compare_config()  # 현재 구성과 병합 후 구성 비교
+    device = driver(IP, 'ccnp', 'cisco')  # 인증 정보와 함께 장비에 대한 연결 객체 생성
+    device.open()  # 장비와의 연결 시작
+    device.load_merge_candidate(filename='Merge1.cfg')  # 'Merge1.cfg' 파일에서 설정 병합 준비
+    diffs = device.compare_config()  # 현재 구성과 병합 후 구성 비교
     if len(diffs) > 0:  # 차이점이 있으면
         print(diffs)  # 차이점 출력
-        switch.commit_config()  # 변경 사항 커밋(적용)
+        device.commit_config()  # 변경 사항 커밋(적용)
     else:  # 차이점이 없으면
         print('No change : ACL, loopback')  # 변경 사항 없음 메시지 출력
-        switch.discard_config()  # 변경 사항 버림
+        device.discard_config()  # 변경 사항 버림
 
-    switch.load_merge_candidate(filename='ospf1.cfg')  # 'ospf1.cfg' 파일에서 OSPF 설정 병합 준비
+    device.load_merge_candidate(filename='ospf1.cfg')  # 'ospf1.cfg' 파일에서 OSPF 설정 병합 준비
 
-    diffs = switch.compare_config()  # 현재 구성과 OSPF 설정 병합 후 구성 비교
+    diffs = device.compare_config()  # 현재 구성과 OSPF 설정 병합 후 구성 비교
     if len(diffs) > 0:  # 차이점이 있으면
         print(diffs)  # 차이점 출력
-        switch.commit_config()  # 변경 사항 커밋(적용)
+        device.commit_config()  # 변경 사항 커밋(적용)
     else:  # 차이점이 없으면
         print('No change : OSPF')  # OSPF 변경 사항 없음 메시지 출력
-        switch.discard_config()  # 변경 사항 버림
+        device.discard_config()  # 변경 사항 버림
 
-    switch.close()  
+    device.close()  
